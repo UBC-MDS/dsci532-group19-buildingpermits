c1ab5d6317651669486a9a219eaef44cc9f5483f
Revathy Ponnambalam <revathyp@student.ubc.ca>
2023-03-17 20:58:11 -0700

    updated testing

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,12 +1,5 @@
 library(shinytest2)
 
-test_that("{shinytest2} recording: test_defaultinput", {
-  app <- AppDriver$new(variant = platform_variant(), name = "test_defaultinput", 
-      height = 975, width = 1619)
-  app$expect_values()
-  app$expect_screenshot()
-})
-
 
 test_that("{shinytest2} recording: test_validoutput", {
   app <- AppDriver$new(name = "test_validoutput", height = 975, width = 1619)
@@ -33,5 +26,21 @@ test_that("{shinytest2} recording: test_validoutput2", {
   app$set_inputs(projectValue = c(67635000, 2.5e+08))
   app$set_inputs(projectValue = c(0, 2.5e+08))
   app$set_inputs(projectValue = c(0, 131780000))
+  app$expect_values()
+})
+
+
+
+
+test_that("{shinytest2} recording: valid_output3", {
+  app <- AppDriver$new(name = "valid_output3", height = 789, width = 1139)
+  app$set_inputs(locations_groups = c("Satellite View", "Basemap", "building locations"), 
+      allow_no_input_binding_ = TRUE)
+  app$expect_values()
+  app$set_inputs(selected_variable = "PermitElapsedDays")
+  app$set_inputs(dateRange = c("2017-01-17", "2023-03-02"))
+  app$set_inputs(dateRange = c("2017-01-17", "2022-08-16"))
+  app$set_inputs(type_of_work = "Addition / Alteration")
+  app$set_inputs(selected_variable = "ProjectValue")
   app$expect_values()
 })
